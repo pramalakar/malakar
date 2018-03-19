6ec02413da138410a60d836fedddd1c2fbf9a96f
Prakash Malakar <pramalakar.2010@gmail.com>
2018-03-19 18:47:13 +1100

    Created GetAllUsers api

--- malakar/Helpers/UserManager.cs
+++ malakar/Helpers/UserManager.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNet.Identity;
 using malakar.Data.Abstract;
 using malakar.Data.Repositories;
 using System;
+using malakar.Models;
+using Microsoft.AspNet.Identity.Owin;
 
 namespace malakar.Helpers
 {
@@ -43,25 +45,11 @@ namespace malakar.Helpers
             }
             return 0;
         }
-        public Models.User GetLoggedInUser()
+
+        public dynamic GetLoggedInUser()
         {
-            if (HttpContext.Current != null && HttpContext.Current.User.Identity.IsAuthenticated)
-            {
-                ClaimsIdentity identity = HttpContext.Current.User.Identity as ClaimsIdentity;
-                //string UserCode = identity.GetUserId();
-                //string UserName = identity.GetUserName();
-                string UserID = identity.FindFirstValue("UserID");
-                try
-                {
-                    var user = _userRepository.GetSingle(Convert.ToInt32(UserID));
-
-                    return user;
-
-                }
-                catch { }
-
-            }
-            return null;
+            ApplicationUser user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+            return user;
         }
     }
 }
