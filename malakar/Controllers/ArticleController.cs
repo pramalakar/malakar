2f4caa3a4a2b99b7449f3ff45461f776c8c42033
Prakash Malakar <pramalakar.2010@gmail.com>
2018-04-01 19:12:25 +1000

    Added try-catch in SearchArticle api

--- malakar/Controllers/ArticleController.cs
+++ malakar/Controllers/ArticleController.cs
@@ -47,36 +47,44 @@ namespace malakar.Controllers
         [Route("api/Article/SearchArticle")]
         public IHttpActionResult searchArticle(SearchArticle searchArticle)
         {
-            var result = (from article in db.Article
-                          from ab in article.ArticleCategoryToArticle
-                          select new
-                          {
-                              ab.ArticleCategoryId,
-                              ab.Article.Id,
-                              ab.Article.Title,
-                              ab.Article.Brief,
-                              ab.Article.Content,
-                              ab.Article.Published,
-                              ab.Article.Date,
-                              ab.Article.Banner,
-                              ab.Article.OwnerId,
-                              //ab.Article.ArticleCategoryToArticle,
-                              ab.Article.StatusID,
-                              ab.Article.DateAdded
-                          }).AsQueryable();
-
-            if (searchArticle != null)
+            try
             {
-                if (searchArticle.CategoryId.HasValue)
-                    result = result.Where(x => x.ArticleCategoryId == searchArticle.CategoryId);
-                if (!string.IsNullOrEmpty(searchArticle.Title))
-                    result = result.Where(x => x.Title.Contains(searchArticle.Title));
-                if (searchArticle.Published.HasValue)
-                    result = result.Where(x => x.Published == searchArticle.Published);
+                var result = (from article in db.Article
+                              from ab in article.ArticleCategoryToArticle
+                              select new
+                              {
+                                  ab.ArticleCategoryId,
+                                  ab.Article.Id,
+                                  ab.Article.Title,
+                                  ab.Article.Brief,
+                                  ab.Article.Content,
+                                  ab.Article.Published,
+                                  ab.Article.Date,
+                                  ab.Article.Banner,
+                                  ab.Article.OwnerId,
+                                  //ab.Article.ArticleCategoryToArticle,
+                                  ab.Article.StatusID,
+                                  ab.Article.DateAdded
+                              }).AsQueryable();
 
+                if (searchArticle != null)
+                {
+                    if (searchArticle.CategoryId.HasValue)
+                        result = result.Where(x => x.ArticleCategoryId == searchArticle.CategoryId);
+                    if (!string.IsNullOrEmpty(searchArticle.Title))
+                        result = result.Where(x => x.Title.Contains(searchArticle.Title));
+                    if (searchArticle.Published.HasValue)
+                        result = result.Where(x => x.Published == searchArticle.Published);
+
+                }
+
+                return Ok(result);
             }
-            
-            return Ok(result);
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
         }
 
     }
