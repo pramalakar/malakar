3f087f3b92ca92c1b84f7ca764fb00bf213a9561
Prakash Malakar <pramalakar.2010@gmail.com>
2018-04-01 19:05:02 +1000

    Added SearchArticle api

--- malakar/Controllers/ArticleController.cs
+++ malakar/Controllers/ArticleController.cs
@@ -1,4 +1,5 @@
 ﻿using malakar.Data;
+using malakar.Models.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -39,6 +40,43 @@ namespace malakar.Controllers
                              ab.Article.DateAdded
                          });
             return Ok(result.ToList());
+        }
+
+        //POST /api/Article/GetArticlesByCategory?id=1
+        [HttpPost]
+        [Route("api/Article/SearchArticle")]
+        public IHttpActionResult searchArticle(SearchArticle searchArticle)
+        {
+            var result = (from article in db.Article
+                          from ab in article.ArticleCategoryToArticle
+                          select new
+                          {
+                              ab.ArticleCategoryId,
+                              ab.Article.Id,
+                              ab.Article.Title,
+                              ab.Article.Brief,
+                              ab.Article.Content,
+                              ab.Article.Published,
+                              ab.Article.Date,
+                              ab.Article.Banner,
+                              ab.Article.OwnerId,
+                              //ab.Article.ArticleCategoryToArticle,
+                              ab.Article.StatusID,
+                              ab.Article.DateAdded
+                          }).AsQueryable();
+
+            if (searchArticle != null)
+            {
+                if (searchArticle.CategoryId.HasValue)
+                    result = result.Where(x => x.ArticleCategoryId == searchArticle.CategoryId);
+                if (!string.IsNullOrEmpty(searchArticle.Title))
+                    result = result.Where(x => x.Title.Contains(searchArticle.Title));
+                if (searchArticle.Published.HasValue)
+                    result = result.Where(x => x.Published == searchArticle.Published);
+
+            }
+            
+            return Ok(result);
         }
 
     }
---
+++ malakar/Models/Helpers/SearchArticle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace malakar.Models.Helpers
+{
+    public class SearchArticle
+    {
+        public int? CategoryId { get; set; }
+
+        public string Title { get; set; }
+
+        public bool? Published { get; set; }
+    }
+}
